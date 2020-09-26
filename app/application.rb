1234523d85e4c1c06623a6d0d9ca4f8cb29efc15
Richard Kurwitz <kurwitz3@aol.com>
2020-09-26 00:30:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,9 @@
 class Application 
   def call(env)
     resp =  Rack::Response.new 
+    
     time = Time.new
-if time.hour >= 9 && time.hour < 19
+
+   if time.hour > 9 && time.hour < 19
     // Your code
 end
